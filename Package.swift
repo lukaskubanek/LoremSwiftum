0f3e2bbd7ff9e1cc38fabee51b3a2254cdf4f921
toshi0383 <t.suzuki326@gmail.com>
2019-10-31 21:24:57 +0900

    support SwiftPM while adding Example target

--- Package.swift
+++ Package.swift
@@ -1,26 +1,15 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// Managed by ice
 
 import PackageDescription
 
 let package = Package(
     name: "LoremSwiftum",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "LoremSwiftum",
-            targets: ["LoremSwiftum"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "LoremSwiftum", targets: ["LoremSwiftum"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "LoremSwiftum",
-            dependencies: [],
-            path: "Sources"),
+        .target(name: "LoremSwiftum", dependencies: []),
+        .target(name: "Example", dependencies: ["LoremSwiftum"]),
     ]
 )
---
+++ Sources/Example/main.swift
@@ -0,0 +1,13 @@
+import LoremSwiftum
+
+print("[word] \(Lorem.word)")
+print("[sentence] \(Lorem.sentence)")
+print("[paragraph] \(Lorem.paragraph)")
+print("[title] \(Lorem.title)")
+print("[firstName] \(Lorem.firstName)")
+print("[lastName] \(Lorem.lastName)")
+print("[fullName] \(Lorem.fullName)")
+print("[emailAddress] \(Lorem.emailAddress)")
+print("[url] \(Lorem.url)")
+print("[shortTweet] \(Lorem.shortTweet)")
+print("[tweet] \(Lorem.tweet)")
